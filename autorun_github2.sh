a79fdeaebf3cfcc5085995e225c2694aee086797
AqFad <85995579+AqFad2811@users.noreply.github.com>
2022-10-18 20:41:29 +0800

    Delete autorun_github2.sh

--- autorun_github2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo $(dirname $0)
-cd $(dirname $0)/scripts/
-python3 dailymotion_m3ugrabber2.py > ../KompasTV.m3u8
-#python3 dailymotion_m3ugrabber.py "212.129.57.194:41258" > ../dailymotion-EU.m3u
-
-echo m3u grabbed
